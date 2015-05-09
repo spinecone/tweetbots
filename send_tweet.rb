9ebbec1618c77378c5968ef56c8d69128ec89acf
Nathaniel Smith <nathanielksmith@gmail.com>
2015-05-09 02:01:33 -0700

    handle case where no tweet is generated

--- send_tweet.rb
+++ send_tweet.rb
@@ -1,8 +1,14 @@
 require 'chatterbot'
+require './tweet.rb'
 require './ceasedesistbot.rb'
 
 bot = Ceasedesistbot.new
-bot.home_timeline(count: 1) do |tweet|
-  reply TweetGenerator.new(tweet.text).generate_tweet, tweet
-  bot.config[:since_id] = tweet.id
+bot.home_timeline do |original_tweet|
+  new_tweet_text = TweetGenerator.new(original_tweet.text).generate_tweet
+
+  if !new_tweet_text.nil?
+    bot.reply new_tweet_text, original_tweet
+    bot.config[:since_id] = original_tweet.id
+    break
+  end
 end
