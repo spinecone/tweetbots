9190d7c1fd8b9a5f9b650b343a00b54ea4967491
Terian Koscik <terian@kongregate.com>
2015-05-09 02:38:04 -0700

    fixed check for nil tweet

--- send_tweet.rb
+++ send_tweet.rb
@@ -7,6 +7,8 @@ bot.home_timeline do |original_tweet|
   new_tweet_text = TweetGenerator.new(original_tweet.text, original_tweet.user.screen_name).generate_tweet
 
   bot.config[:since_id] = original_tweet.id
-  break unless new_tweet_text.nil?
-  bot.reply new_tweet_text, original_tweet
+  unless new_tweet_text.nil?
+    bot.reply new_tweet_text, original_tweet
+    break
+  end
 end
