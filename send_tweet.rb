d4dd43457182fffc5d574290c876229c27d261ad
Terian Koscik <terian@kongregate.com>
2015-05-09 22:01:37 -0700

    don't reply to self

--- send_tweet.rb
+++ send_tweet.rb
@@ -4,10 +4,11 @@ require './ceasedesistbot.rb'
 
 bot = Ceasedesistbot.new
 bot.home_timeline do |original_tweet|
-  new_tweet_text = TweetGenerator.new(original_tweet.text, original_tweet.user.screen_name).generate_tweet
+  new_tweet_username = original_tweet.user.screen_name
+  new_tweet_text = TweetGenerator.new(original_tweet.text, new_tweet_username).generate_tweet
 
   bot.config[:since_id] = original_tweet.id
-  unless new_tweet_text.nil? || original_tweet.retweet?
+  unless new_tweet_text.nil? || original_tweet.retweet? || new_tweet_username == bot.botname
     bot.reply new_tweet_text, original_tweet
     break
   end
