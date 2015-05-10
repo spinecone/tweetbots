54fb9d561795e3858ca98c904a9110c7e8d2e31f
Terian Koscik <terian@kongregate.com>
2015-05-09 21:47:35 -0700

    don't reply to retweets

--- send_tweet.rb
+++ send_tweet.rb
@@ -7,7 +7,7 @@ bot.home_timeline do |original_tweet|
   new_tweet_text = TweetGenerator.new(original_tweet.text, original_tweet.user.screen_name).generate_tweet
 
   bot.config[:since_id] = original_tweet.id
-  unless new_tweet_text.nil?
+  unless new_tweet_text.nil? || original_tweet.retweet?
     bot.reply new_tweet_text, original_tweet
     break
   end
