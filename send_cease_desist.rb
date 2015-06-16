361fae7fdbc24a5b69f7a68e5724a67407b4527f
Terian Koscik <terian@kongregate.com>
2015-06-16 14:28:03 -0700

    fixed reference to old class name

--- send_cease_desist.rb
+++ send_cease_desist.rb
@@ -12,7 +12,7 @@ log.debug "Starting bot"
 bot.home_timeline do |original_tweet|
   log.debug "Trying to generate based on tweet id #{original_tweet.id}"
   new_tweet_username = original_tweet.user.screen_name
-  new_tweet_text = TweetGenerator.new(original_tweet.text, new_tweet_username).generate_tweet
+  new_tweet_text = CeaseDesistGenerator.new(original_tweet.text, new_tweet_username).generate_tweet
 
   bot.config[:since_id] = original_tweet.id
   unless new_tweet_text.nil? || original_tweet.retweet? || new_tweet_username == bot.botname
